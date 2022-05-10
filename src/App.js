a84cd66df4e2edb8248450f851f8820ed2cfa7e9
tayfundagci <tayfundagci3517@gmail.com>
2022-05-10 15:52:27 +0300

    change bgcolor

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen bg-gray-100">
       <div className="container mx-auto px-2 max-w-5xl pt-10 md:pt-32 ">
         <h1 className="text-center font-bold text-2xl text-gray-700 mb-5">
           CRUD with redux toolkit
